4d534d621a0f1adefc2f78ef2de48a4480cb8bf9
aswaff <44035215+aswaff@users.noreply.github.com>
2020-04-17 11:02:38 -0400

    Changed colors, and added text

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,25 +2,27 @@ import uuidv1 from 'uuid/v1';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Avery Swafford | Developer', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Full Stack Web Dev', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: 'Hi, my name is',
+  name: 'Avery Swafford',
+  subtitle: 'Node, React, and Javascript Developer',
+  cta: 'About Me',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
+  paragraphOne:
+    "I have been in the information technology industry for over 7 years.  I've worked as a help desk person, performing OS installs, virus removal and laptop and desk hardware repair.  I eventually worked my may to become a network engineer and consultant, managing over 150 endpoints and and around 12 servers.  I now assist large and major accounts with IT and software solutions, while maintaining client relationships.",
+  paragraphTwo:
+    'More recently, I am a self taught full-stack Web Developer.  I have experience in Node.js, React, JavaScript, and CSS/HTML.  As you can see below with my projects, I am a highly motivated and focused developer, that loves to learn new things and ask questions.',
+  // paragraphThree: '',
   resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
 };
 
